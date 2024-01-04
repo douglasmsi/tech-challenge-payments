5459e7f4d6abcc5d010b7740bfb878e951c82ab4
douglasmsi <douglas.marra@mercadolivre.com>
2024-01-04 20:34:00 -0300

    refactor

--- src/main/java/br/com/fiap/postech/fastfood/pagamento/feign/PedidoClient.java
+++ src/main/java/br/com/fiap/postech/fastfood/pagamento/feign/PedidoClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "pedido-service", url = "http://localhost:8082")
+@FeignClient(name = "tech-challenge-order")
 public interface PedidoClient {
 
   @GetMapping
